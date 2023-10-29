92c279e5e0132c76d8d8272c5a211910ddc90fe0
Samantha Siqueira <samantharsiqueira@gmail.com>
2023-10-29 09:47:44 +0000

    first one done

--- src/1-unique-products-amount.js
+++ src/1-unique-products-amount.js
@@ -2,6 +2,9 @@ const stockProducts = require('./data.json');
 
 const getUniqueProductsAmount = () => {
   // Desenvolva seu código dentro dessa função...
+ let uniqueProduct = 0;
+ uniqueProduct = stockProducts.length;
+    return uniqueProduct;
 };
 
 module.exports = { getUniqueProductsAmount };
