c0690a43890935e7a5d65240eda6d2b0a4018503
Samantha Siqueira <samantharsiqueira@gmail.com>
2023-10-30 14:08:32 +0000

    terceiro exercicio

--- src/3-out-of-stock-products.js
+++ src/3-out-of-stock-products.js
@@ -1,7 +1,16 @@
 const stockProducts = require('./data.json');
-console.log(stockProducts);
+
 const getOutOfStockProducts = () => {
   // Desenvolva seu código dentro dessa função...
+  let newArray = [];
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    if (stockProducts[index].quantityInStock === 0) {
+      newArray.push(stockProducts[index].productName)
+    }
+  }
+  return newArray;
 };
+
+console.log(getOutOfStockProducts())
 
 module.exports = { getOutOfStockProducts };
