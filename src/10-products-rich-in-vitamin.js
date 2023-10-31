f79e9f0c227d8c6689b6562c23b92ac7e666c215
Samantha Siqueira <samantharsiqueira@gmail.com>
2023-10-31 11:57:45 +0000

    ultimo exercicio

--- src/10-products-rich-in-vitamin.js
+++ src/10-products-rich-in-vitamin.js
@@ -2,6 +2,19 @@ const stockProducts = require('./data.json');
 
 const getProductsRichInVitamin = () => {
   // Desenvolva seu código dentro dessa função...
+  let emptyArray = [];
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    if (stockProducts[index].nutritionalInfo.vitamins) {
+      emptyArray.push({
+        description: `${stockProducts[index].description}`,
+        formattedPrice: `R$ ${stockProducts[index].price.toFixed(2)}`,
+        vitaminsInformation: Object.entries(stockProducts[index].nutritionalInfo.vitamins),
+      });
+      console.log(Object.entries(stockProducts[index].nutritionalInfo.vitamins))
+    }
+  }
+  return emptyArray;
 };
 
+console.log(getProductsRichInVitamin())
 module.exports = { getProductsRichInVitamin };
