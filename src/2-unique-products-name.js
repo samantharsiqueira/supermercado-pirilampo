d7ed6a0a0503959bb825dbee77cc1a0eda2e1c39
Samantha Siqueira <samantharsiqueira@gmail.com>
2023-10-31 11:59:32 +0000

    corrigindo o lint

--- src/2-unique-products-name.js
+++ src/2-unique-products-name.js
@@ -4,10 +4,9 @@ const getUniqueProductsName = () => {
   // Desenvolva seu código dentro dessa função...
   let newArray = [];
   for (let index = 0; index < stockProducts.length; index += 1) {
-  newArray.push(stockProducts[index].productName)
+    newArray.push(stockProducts[index].productName);
   }
   return newArray;
 };
-
 
 module.exports = { getUniqueProductsName };
