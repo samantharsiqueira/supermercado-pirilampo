9f89bdef2a0adf3c451ee1a2e4607999e9769ec3
Samantha Siqueira <samantharsiqueira@gmail.com>
2023-10-31 11:36:17 +0000

    nono exercicio

--- src/9-products-with-allergy-or-intolerance-info.js
+++ src/9-products-with-allergy-or-intolerance-info.js
@@ -2,8 +2,18 @@ const stockProducts = require('./data.json');
 
 const getProductsWithAllergyOrIntoleranceInfo = () => {
   // Desenvolva seu código dentro dessa função...
+  let emptyArray = [];
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    if (stockProducts[index].allergyOrIntolerance) {
+      emptyArray.push({
+        description: `${stockProducts[index].description}`,
+        formattedPrice: `R$ ${stockProducts[index].price.toFixed(2)}`,
+        allergyOrIntoleranceMessage: `Pode conter:
+         ${stockProducts[index].allergyOrIntolerance.join(' ')}`,
+      });
+    }
+  }
+  return emptyArray;
 };
-
-console.log(getProductsWithAllergyOrIntoleranceInfo());
 
 module.exports = { getProductsWithAllergyOrIntoleranceInfo };
